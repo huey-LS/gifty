ea3cdefdf5807cfebe3b0727ad74ef90c8bb9593
ignous-e40 <lizhygm@sina.com>
2014-06-27 11:39:15 +0800

    datadepot fixed

--- datadepot/datadepot.js
+++ datadepot/datadepot.js
@@ -3,116 +3,98 @@
 
   @method set(data_name, data_value)
   @method get(data_name)
-
-
-  format{
-    text: 
-    img: 
-    keyword: 
-    subject:
-  }
+  @method getone(data_name)
 ****************/
 var datadepot = function(){
-  var format = {
-    text: 'String',
-    img: 'Image',
-    keyword: 'String',
-    subject: 'String',
-    checked: 'Boolean'
-  },
-  datas = [];
-
-  function isType(data, type){
-    if(type === 'Function'){
-      return typeof(data) == 'function';
-    }else if(type === 'String'){
-      return typeof(data) === 'string';
-    }
+  this.datas = [];
+}
+datadepot.prototype.isType = function(data, type){
+  if(type === 'Function'){
+    return typeof(data) === 'function';
+  }else if(type === 'String'){
+    return typeof(data) === 'string';
+  }else{
+    return false;
   }
-
-  function defaultFind(data, find){
-    var i;
-    for(i in find){
-      if(data[i] === undefined || find[i] !== data[i]){
-        return false;
-      }
-    }
-    return true;
-  }
-
-  function get(find){
-    var r = [],
-      t,
-      i=0;
-    if(!find){
-      return datas;
-    }else if(isType(find, 'Function')){
-      for(i=0,len=datas.length;i<len;i++){
-        if(find(t=datas[i])) r[r.length] = t;
-      }
-      return r;
-    }else{
-      for(i=0,len=datas.length;i<len;i++){
-        if(defaultFind(t=datas[i], find)) r[r.length] = t;
-      }
-      return r;
-    }
-  }
-  function getone(find){
-    var t,
-      i=0;
-    if(!find){
-      return datas[0];
-    }else if(isType(find, 'Function')){
-      for(i=0,len=datas.length;i<len;i++){
-        if(find(t=datas[i])){
-          return t;
-        }
-      }
-      return undefined;
-    }else{
-      for(i=0,len=datas.length;i<len;i++){
-        if(defaultFind(t=datas[i], find)){
-          return t;
-        }
-      }
-      return undefined;
-    }
-  }
-
-  function add(data){
-    datas[datas.length] = data;
-    return true;
-  }
-
-  function replace(find, data){
-    var one = getone(find),
-      i;
-    if(one){
-      for(i in data){
-        one[i] = data[i];
-      }
-      return true;
-    }else{
+}
+datadepot.prototype._defaultFind = function(data, find){
+  var i;
+  for(i in find){
+    if(data[i] === undefined || find[i] !== data[i]){
       return false;
     }
   }
-
-  function set(){
-    var find,data;
-    if(arguments.length === 0) return false;
-    if(arguments.length === 1){
-      data = arguments[0];
-      return add(data);
-    }else if(arguments.length === 2){
-      find = arguments[0];
-      data = arguments[1];
-      return replace(find, data);
+  return true;
+}
+datadepot.prototype.get = function(find){
+  var _self = this,
+    r = [],
+    t,
+    i=0;
+  if(!find){
+    return _self.datas;
+  }else if(_self.isType(find, 'Function')){
+    for(i=0,len=_self.datas.length;i<len;i++){
+      if(find(t=_self.datas[i])) r[r.length] = t;
     }
+    return r;
+  }else{
+    for(i=0,len=_self.datas.length;i<len;i++){
+      if(_self._defaultFind(t=_self.datas[i], find)) r[r.length] = t;
+    }
+    return r;
   }
-  return {
-    set: set,
-    get: get,
-    getone: getone
+}
+datadepot.prototype.getone = function(find){
+  var t,
+    i=0;
+  if(!find){
+    return datas[0];
+  }else if(_self.isType(find, 'Function')){
+    for(i=0,len=_self.datas.length;i<len;i++){
+      if(find(t=_self.datas[i])){
+        return t;
+      }
+    }
+    return undefined;
+  }else{
+    for(i=0,len=_self.datas.length;i<len;i++){
+      if(_self._defaultFind(t=_self.datas[i], find)){
+        return t;
+      }
+    }
+    return undefined;
   }
-};
+}
+
+datadepot.prototype._add = function(data){
+  datas[datas.length] = data;
+  return true;
+}
+
+datadepot.prototype._updata = function(find, data){
+  var one = getone(find),
+    i;
+  if(one){
+    for(i in data){
+      one[i] = data[i];
+    }
+    return true;
+  }else{
+    return false;
+  }
+}
+
+datadepot.prototype.set = function(){
+  var _self = this,
+    find,data;
+  if(arguments.length === 0) return false;
+  if(arguments.length === 1){
+    data = arguments[0];
+    return _self._add(data);
+  }else if(arguments.length === 2){
+    find = arguments[0];
+    data = arguments[1];
+    return _self._updata(find, data);
+  }
+}
